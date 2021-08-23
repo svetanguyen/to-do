4bb0ed4154864fce65e9eed1e39d7ef61883087c
svetanguyen <matematicaforeber@gmail.com>
2021-08-23 19:57:59 +0700

    add new components

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="task-form">
+    <form>
+      <input class="full-width" type="text" required v-model="title" id="Title" placeholder="Add new tasks...">
+      <label for="Description">Description</label>
+      <textarea class="full-width" name="Description" v-model="description" id="Description"></textarea>
+      <div class="date-wrapper half-width">
+        <label for="DueDate">Due date</label>
+        <input type="date" id="DueDate" name="DueDate"
+       v-model="currentDate"
+       :min="currentDate">
+      </div>
+      <div class="priority-wrapper half-width">
+        <label for="Priority">Priority</label>
+        <select class="half-width" name="Priority" id="Priority">
+          <option value="Low">Low</option>
+          <option value="Normal" selected>Normal</option>
+          <option value="High">High</option>
+        </select>
+      </div>
+      <button class="full-width" type="submit">{{ submit }}</button>
+       
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task form',
+  data() {
+    return {
+      title: this.taskData.title ? this.taskData.title : '',
+      description: this.taskData ? this.taskData.description : ''
+    }
+  },
+  props: ['submit', 'taskData'],
+  computed: {   
+    currentDate() {
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1; //January is 0!
+      var yyyy = today.getFullYear();
+      if(dd<10){
+              dd='0'+dd
+          } 
+          if(mm<10){
+              mm='0'+mm
+          } 
+
+      today = yyyy+'-'+mm+'-'+dd;
+      return today
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  input { 
+    color: #000000;
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 12px;
+    font-weight: normal;
+    font-style: normal;
+    border: 1px solid #BDBDBD;
+    padding: 10px;
+    border-radius: 5px;
+    &::placeholder {
+      color: #000000;
+      font-family: 'Source Sans Pro', sans-serif;
+      font-size: 12px;
+      font-weight: normal;
+      font-style: normal;
+    }
+  }
+  label {
+    text-transform: capitalize;
+    font-family: 'Source Sans Pro';
+    font-size: 12px;
+    font-weight: bold;
+    font-style: normal;
+    text-decoration: none;
+    margin: 15px 0 5px;
+    display: block;
+  }
+  textarea {
+    font-size: 12px;
+    border: 1px solid #000000;
+    height: 125px;
+  }
+  form {
+    text-align: left;
+  }
+  button {
+    text-transform: capitalize;
+    background: #5CB85C;
+    color: #fff;
+    border-radius: 10px;
+    border: none;
+    font-family: Arimo;
+    font-size: 15px;
+    padding: 10px;
+    margin-top: 70px;
+  }
+  .half-width {
+    display: inline-block;
+    input,
+    select {
+      border: 1px solid #000000;
+      font-size: 12px;
+      height: 31px;
+      border-radius: 0;
+    }
+  }
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="to-do-wrapper">
+   <div class="new-task-wrapper column">
+     <h2>New task</h2>
+    <TaskForm :submit="add" />
+   </div>
+   <div class="to-do-list column">
+     <h2>To do list</h2>
+   </div>
+  </div>
+</template>
+
+<script>
+import TaskForm from './TaskForm.vue'
+export default {
+  name: 'To-do',
+  data() {
+    return {
+      add: 'add',
+      update: 'update'
+    }
+  },
+  components: {
+    TaskForm
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.full-width {
+  width: 100%;
+}
+.half-width {
+  width: 50%;
+}
+.column {
+  border: 1px solid #000;
+  padding: 50px;
+}
+.to-do-wrapper {
+  h2 {
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 18px;
+    text-align: center;
+    text-transform: capitalize;
+    margin-bottom: 50px;
+    font-weight: 600;
+  }
+  @media screen and (min-width: 749px) {
+    display: flex;
+    &>div {
+      width: 50%;
+    }
+    
+  }
+} 
+</style>
