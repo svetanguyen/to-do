a642036518a898d742614d6a9c5513adbc0716ee
svetanguyen <matematicaforeber@gmail.com>
2021-08-23 22:08:10 +0700

    submit new task

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="task-form">
-    <form>
+    <form @submit.prevent="submitTask">
       <input class="full-width" type="text" required v-model="title" id="Title" placeholder="Add new tasks...">
       <label for="Description">Description</label>
       <textarea class="full-width" name="Description" v-model="description" id="Description"></textarea>
       <div class="date-wrapper half-width">
-        <label for="DueDate">Due date</label>
-        <input type="date" id="DueDate" name="DueDate"
-       v-model="dueDate"
-       :min="currentDate">
+        <label for="DueDate">Due date</label>        
+       <input :show-current="false" type="date" id="dateId" name="DueDate" v-model="date"
+       :min="currentDate" >
       </div>
       <div class="priority-wrapper half-width">
         <label for="Priority">Priority</label>
-        <select v-model="selected" class="half-width" name="Priority" id="Priority">
+        <select v-model="priority" @change="selectedValue" class="half-width" name="Priority" id="Priority">
           <option value="low">Low</option>
           <option value="normal" selected>Normal</option>
           <option value="high">High</option>
@@ -26,16 +25,24 @@
 
 <script>
 export default {
-  name: 'Task form',
-  data() {
+  name: 'Task-form',
+  data: function() {
     return {
-      title: this.task.title ? this.task.title : '',
+      title: this.task ? this.task.title : '',
       description: this.task ? this.task.description : '',
-      dueDate: this.currentDate
+      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      showCalendar: false,
+      priority: 'normal'
     }
   },
-  props: ['submit', 'task'],
+  props: ['submit', 'task', 'id'],  
   computed: {   
+    dateId() {
+      return `DueDate--${this.id}`
+    },
+    themeColor() {
+      return this.$vuetify.theme.themes
+    },
     currentDate() {
       var today = new Date();
       var dd = today.getDate();
@@ -50,13 +57,58 @@ export default {
 
       today = yyyy+'-'+mm+'-'+dd;
       return today
+    },
+    dateNow() {
+      return this.date
+    }
+   
+  },
+  methods: {
+    changeDate(date) {
+      var dd = date.getDate();
+      var mm = date.getMonth()+1; //January is 0!
+      var yyyy = date.getFullYear();
+      if(dd<10){
+              dd='0'+dd
+          } 
+          if(mm<10){
+              mm='0'+mm
+          } 
+
+      date = yyyy+'-'+mm+'-'+dd;
+      return date
+    },
+    toggleCalendar() {
+      this.showCalendar = !this.showCalendar;
+    },
+    selectedValue(e) {
+      this.priority = e.target.value
+    },
+    submitTask() {
+      const newTask = {
+        title: this.title,
+        description: this.description,
+        date: this.date,
+        priority: this.priority,
+        checked: this.checked
+      }
+      if (this.submit === 'add') {
+        this.$store.state.tasks.push(newTask)
+        this.title = ''
+        this.description = ''
+        this.date = (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        this.priority = 'normal'
+        this.checked = false  
+      }
+      
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-  input { 
+<style lang="scss">
+.task-form {
+input { 
     color: #000000;
     font-family: 'Source Sans Pro', sans-serif;
     font-size: 12px;
@@ -92,7 +144,7 @@ export default {
   form {
     text-align: left;
   }
-  button {
+  button.full-width {
     text-transform: capitalize;
     background: #5CB85C;
     color: #fff;
@@ -113,4 +165,16 @@ export default {
       border-radius: 0;
     }
   }
+  .task-form {
+      padding: 30px;
+  }
+  .v-date-picker-title {
+    background: blue;
+    padding: 20px;
+  }
+  .v-date-picker-table .v-btn.v-btn--active {
+    color: blue;
+  }
+}
+  
 </style>
